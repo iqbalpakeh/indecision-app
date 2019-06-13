dac4afbd373148bc41c2d898085e76a54b20e440
Progrema Studio <iqbalpakeh@gmail.com>
2019-06-14 05:15:08 +0700

    Remove file from repo

--- public/scripts/app.js
+++
@@ -1,7 +0,0 @@
-console.log("App.js is running!");
-
-// JSX - Javascript XML
-var template = <p>This is JSX from App.js</p>;
-var appRoot = document.getElementById("app");
-
-ReactDOM.render(template, appRoot);
