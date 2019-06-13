b2e77b111a182f66f4ebf18680fc7e837c818642
Progrema Studio <iqbalpakeh@gmail.com>
2019-06-14 05:44:41 +0700

    Add live-server command

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 //------------------------------------------------------------------------
+// Use Live-Server to automatically load new page to browser
+// $ live-server public
+//
 // Use babel to compile with react and modern ES support:
 // $ babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
 //------------------------------------------------------------------------
@@ -6,7 +9,7 @@
 console.log("App.js is running!");
 
 // JSX - Javascript XML
-var template = <p>This is JSX from App.js</p>;
+var template = <p>Does this change?</p>;
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
