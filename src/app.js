10b6dc69c46ef659c8617394651a172a547e8d15
Progrema Studio <iqbalpakeh@gmail.com>
2019-06-14 11:04:21 +0700

    Update template

--- src/app.js
+++ src/app.js
@@ -8,8 +8,18 @@
 
 console.log("App.js is running!");
 
-// JSX - Javascript XML
-var template = <p>Does this change?</p>;
+var template = (
+	<div>
+		<h1>Indecision App</h1>
+		<p>This is some information</p>
+		<ol>
+			<li>Item One</li>
+			<li>Item Two</li>
+			<li>Item Three</li>
+		</ol>
+	</div>
+);
+
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
