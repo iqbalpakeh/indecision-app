87b9f7b625ef9ce3f7c869eedbfda3f96abecffa
Progrema Studio <iqbalpakeh@gmail.com>
2019-06-16 05:50:19 +0700

    remove template two

--- src/app.js
+++ src/app.js
@@ -26,26 +26,5 @@ const template = (
 	</div>
 );
 
-const user = {
-	name: "Iqbal Pakeh",
-	age: 19,
-	location: "Medan"
-};
-
-function getLocation(location) {
-	if (location) {
-		return <p>Location: {user.location}</p>;
-	}
-}
-
-const templateTwo = (
-	<div>
-		<h1>{user.name ? user.name : "Anonymous"}</h1>
-		{user.age && user.age >= 18 && <p>Age: {user.age}</p>}
-		{getLocation(user.location)}
-	</div>
-);
-
 const appRoot = document.getElementById("app");
-
 ReactDOM.render(template, appRoot);
