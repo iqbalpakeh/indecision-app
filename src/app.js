c93d2ffa117cfe899d7c1c12adea527486a04c23
Progrema Studio <iqbalpakeh@gmail.com>
2019-06-17 04:05:31 +0700

    Remove counter app

--- src/app.js
+++ src/app.js
@@ -26,39 +26,4 @@ const template = (
 	</div>
 );
 
-let count = 0;
-
-const addOne = () => {
-	count = count + 1;
-	renderCounterApp();
-};
-
-const minusOne = () => {
-	count = count - 1;
-	renderCounterApp();
-};
-
-const reset = () => {
-	count = 0;
-	renderCounterApp();
-};
-
 const appRoot = document.getElementById("app");
-
-/**
- * React doesn't have built-in data binding. Thus, if data change, we need
- * to rerender the application manually!!
- */
-const renderCounterApp = () => {
-	const templateTwo = (
-		<div>
-			<h1>Count: {count}</h1>
-			<button onClick={addOne}>+1</button>
-			<button onClick={minusOne}>-1</button>
-			<button onClick={reset}>reset</button>
-		</div>
-	);
-	ReactDOM.render(templateTwo, appRoot);
-};
-
-renderCounterApp();
