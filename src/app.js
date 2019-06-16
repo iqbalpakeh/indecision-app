cbc4ee851ff295171eca10c72040cd252d0b8a9a
Progrema Studio <iqbalpakeh@gmail.com>
2019-06-17 04:34:16 +0700

    Implement render and removeAll methods

--- src/app.js
+++ src/app.js
@@ -8,22 +8,54 @@
 
 console.log("App.js is running!");
 
+const appRoot = document.getElementById("app");
+
 const app = {
 	title: "Indecision App",
 	subtitle: "Put your life in the hands of computer",
-	options: ["One", "Two"]
+	options: []
 };
 
-const template = (
-	<div>
-		<h1>{app.title}</h1>
-		{app.subtitle && <p>{app.subtitle}</p>}
-		<p>{app.options.length > 0 ? "Here are your options:" : "No options"}</p>
-		<ol>
-			<li>Item One</li>
-			<li>Item Two</li>
-		</ol>
-	</div>
-);
+const onFormSubmit = e => {
+	// this will prevent default behavious which
+	// in this case is full page refresh!
+	e.preventDefault();
 
-const appRoot = document.getElementById("app");
+	// retrieve user value from option
+	const option = e.target.elements.option.value;
+
+	if (option) {
+		app.options.push(option);
+		e.target.elements.option.value = "";
+		render();
+	}
+};
+
+const onRemoveAll = () => {
+	app.options = [];
+	render();
+};
+
+const render = () => {
+	const template = (
+		<div>
+			<h1>{app.title}</h1>
+			{app.subtitle && <p>{app.subtitle}</p>}
+			<p>{app.options.length > 0 ? "Here are your options:" : "No options"}</p>
+			<p>{app.options.length}</p>
+			<button onClick={onRemoveAll}>Remove All</button>
+			<ol>
+				<li>Item One</li>
+				<li>Item Two</li>
+			</ol>
+			<form onSubmit={onFormSubmit}>
+				<input type="text" name="option" />
+				<button>Add Option</button>
+			</form>
+		</div>
+	);
+
+	ReactDOM.render(template, appRoot);
+};
+
+render();
