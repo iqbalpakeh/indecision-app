541f61710fd38b86c88033a49c231cdad002c106
Progrema Studio <iqbalpakeh@gmail.com>
2019-06-17 03:53:04 +0700

    Add data binding

--- src/app.js
+++ src/app.js
@@ -29,25 +29,36 @@ const template = (
 let count = 0;
 
 const addOne = () => {
-	console.log("addOne");
+	count = count + 1;
+	renderCounterApp();
 };
 
 const minusOne = () => {
-	console.log("minusOne");
+	count = count - 1;
+	renderCounterApp();
 };
 
 const reset = () => {
-	console.log("reset");
+	count = 0;
+	renderCounterApp();
 };
 
-const templateTwo = (
-	<div>
-		<h1>Count: {count}</h1>
-		<button onClick={addOne}>+1</button>
-		<button onClick={minusOne}>-1</button>
-		<button onClick={reset}>reset</button>
-	</div>
-);
-
 const appRoot = document.getElementById("app");
-ReactDOM.render(templateTwo, appRoot);
+
+/**
+ * React doesn't have built-in data binding. Thus, if data change, we need
+ * to rerender the application manually!!
+ */
+const renderCounterApp = () => {
+	const templateTwo = (
+		<div>
+			<h1>Count: {count}</h1>
+			<button onClick={addOne}>+1</button>
+			<button onClick={minusOne}>-1</button>
+			<button onClick={reset}>reset</button>
+		</div>
+	);
+	ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
