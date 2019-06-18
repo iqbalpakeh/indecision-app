3895aeba1a7a609fe3f033a067cb5102495c799c
Progrema Studio <iqbalpakeh@gmail.com>
2019-06-19 04:48:26 +0700

    Implement event handler function

--- src/app.js
+++ src/app.js
@@ -35,19 +35,28 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+	handlePick() {
+		alert("handlePick");
+	}
+
 	render() {
 		return (
 			<div>
-				<button>What should I do?</button>
+				<button onClick={this.handlePick}>What should I do?</button>
 			</div>
 		);
 	}
 }
 
 class Options extends React.Component {
+	handleRemoveAll() {
+		alert("removeAll");
+	}
+
 	render() {
 		return (
 			<div>
+				<button onClick={this.handleRemoveAll}>Remove All</button>
 				{this.props.options.map(option => (
 					<Option key={option} optionText={option} />
 				))}
@@ -63,8 +72,23 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+	handleAddOption(e) {
+		e.preventDefault();
+		const option = e.target.elements.option.value;
+		if (option) {
+			alert(option);
+		}
+	}
+
 	render() {
-		return <div>AddOption component here</div>;
+		return (
+			<div>
+				<form onSubmit={this.handleAddOption}>
+					<input type="text" name="option" />
+					<button>Add Option</button>
+				</form>
+			</div>
+		);
 	}
 }
 
