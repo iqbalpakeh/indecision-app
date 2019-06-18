437342154d9cc9f2273a05607bf7a7e04c3d48a8
Progrema Studio <iqbalpakeh@gmail.com>
2019-06-19 04:07:23 +0700

    Add title and subtitle props

--- src/app.js
+++ src/app.js
@@ -8,9 +8,11 @@
 
 class IndecisionApp extends React.Component {
 	render() {
+		const title = "Indecision App";
+		const subtitle = "Put your life in the hands of a computer.";
 		return (
 			<div>
-				<Header />
+				<Header title={title} subtitle={subtitle} />
 				<Action />
 				<Options />
 				<AddOption />
@@ -23,8 +25,8 @@ class Header extends React.Component {
 	render() {
 		return (
 			<div>
-				<h1>Indecision App</h1>
-				<h2>Put your life in the hands of a computer.</h2>
+				<h1>{this.props.title}</h1>
+				<h2>{this.props.subtitle}</h2>
 			</div>
 		);
 	}
