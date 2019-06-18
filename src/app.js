be1fe7b2bcb9aabcc0f7dec0e9a5f912bccf852c
Progrema Studio <iqbalpakeh@gmail.com>
2019-06-19 04:54:28 +0700

    Add trim() to clear the string on form

--- src/app.js
+++ src/app.js
@@ -74,7 +74,7 @@ class Option extends React.Component {
 class AddOption extends React.Component {
 	handleAddOption(e) {
 		e.preventDefault();
-		const option = e.target.elements.option.value;
+		const option = e.target.elements.option.value.trim();
 		if (option) {
 			alert(option);
 		}
