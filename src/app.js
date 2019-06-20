f61a5f5cc25f190676ff75bda1e709ae5220f850
Progrema Studio <iqbalpakeh@gmail.com>
2019-06-20 19:09:39 +0800

    create state of options and propagate it Action

--- src/app.js
+++ src/app.js
@@ -7,16 +7,21 @@
 //------------------------------------------------------------------------
 
 class IndecisionApp extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			options: ["Thing one", "Thing two", "Thing three"]
+		};
+	}
 	render() {
 		const title = "Indecision App";
 		const subtitle = "Put your life in the hands of a computer.";
-		const options = ["Thing one", "Thing two", "Thing four"];
 
 		return (
 			<div>
 				<Header title={title} subtitle={subtitle} />
-				<Action />
-				<Options options={options} />
+				<Action hasOptions={this.state.options.length > 0} />
+				<Options options={this.state.options} />
 				<AddOption />
 			</div>
 		);
@@ -42,7 +47,9 @@ class Action extends React.Component {
 	render() {
 		return (
 			<div>
-				<button onClick={this.handlePick}>What should I do?</button>
+				<button onClick={this.handlePick} disabled={!this.props.hasOptions}>
+					What should I do?
+				</button>
 			</div>
 		);
 	}
