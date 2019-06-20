15f7d9e3b3309ef6c08a1365728173574b2543b3
Progrema Studio <iqbalpakeh@gmail.com>
2019-06-21 03:26:48 +0700

    Add check of option value to addOption

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ class IndecisionApp extends React.Component {
 		this.handleAddOption = this.handleAddOption.bind(this);
 
 		this.state = {
-			options: ["Thing one", "Thing two", "Thing three"]
+			options: []
 		};
 	}
 
@@ -37,6 +37,12 @@ class IndecisionApp extends React.Component {
 	}
 
 	handleAddOption(option) {
+		if (!option) {
+			return "Enter valid value to add option";
+		} else if (this.state.options.indexOf(option) > -1) {
+			return "This option already exist";
+		}
+
 		this.setState(prevState => {
 			return {
 				options: prevState.options.concat([option])
@@ -129,19 +135,24 @@ class AddOption extends React.Component {
 	constructor(props) {
 		super(props);
 		this.handleAddOption = this.handleAddOption.bind(this);
+		this.state = {
+			error: undefined
+		};
 	}
 
 	handleAddOption(e) {
 		e.preventDefault();
 		const option = e.target.elements.option.value.trim();
-		if (option) {
-			this.props.handleAddOption(option);
-		}
+		const error = this.props.handleAddOption(option);
+		this.setState(() => {
+			return { error };
+		});
 	}
 
 	render() {
 		return (
 			<div>
+				{this.state.error && <p>{this.state.error}</p>}
 				<form onSubmit={this.handleAddOption}>
 					<input type="text" name="option" />
 					<button>Add Option</button>
