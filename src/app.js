408d23b3f01ab4e5ce225e17ac63f09351a4878e
Progrema Studio <iqbalpakeh@gmail.com>
2019-06-22 23:02:38 +0700

    Implement localStorage

--- src/app.js
+++ src/app.js
@@ -21,11 +21,23 @@ class IndecisionApp extends React.Component {
 	}
 
 	componentDidMount() {
-		console.log("fetching data!");
+		try {
+			const json = localStorage.getItem("options");
+			const options = JSON.parse(json);
+
+			if (options) {
+				this.setState(() => ({ options }));
+			}
+		} catch (e) {
+			// do nothing at all
+		}
 	}
 
-	componentDidUpdate() {
-		console.log("saving data!");
+	componentDidUpdate(prevProps, prevState) {
+		if (prevState.options.length !== this.state.options.length) {
+			const json = JSON.stringify(this.state.options);
+			localStorage.setItem("options", json);
+		}
 	}
 
 	componentWillUnmount() {
@@ -112,6 +124,9 @@ const Options = props => {
 	return (
 		<div>
 			<button onClick={props.handleDeleteOptions}>Remove All</button>
+			{props.options.length === 0 && (
+				<p>Please add an option to get started!</p>
+			)}
 			{props.options.map(option => (
 				<Option
 					key={option}
@@ -152,6 +167,9 @@ class AddOption extends React.Component {
 		const option = e.target.elements.option.value.trim();
 		const error = this.props.handleAddOption(option);
 		this.setState(() => ({ error }));
+		if (!error) {
+			e.target.elements.option.value = "";
+		}
 	}
 
 	render() {
