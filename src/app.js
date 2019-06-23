919e7cd22d1ce1aaab797810091ed4aa72db6b91
Progrema Studio <iqbalpakeh@gmail.com>
2019-06-24 03:59:07 +0700

    Uncommented all function call

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-// import { square, add } from "./utils.js";
+import { square, add } from "./utils.js";
 
-// console.log("app.js is running!");
-// console.log(square(4));
-// console.log(add(4, 5));
+console.log("app.js is running!");
+console.log(square(4));
+console.log(add(4, 5));
 
 import { isAdult, canDrink } from "./person.js";
 
