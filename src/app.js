82232750b692e6fba09142aa39977974410f1a44
Progrema Studio <iqbalpakeh@gmail.com>
2019-06-24 04:56:53 +0700

    Use default export

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
-import { square, add } from "./utils.js";
+import subtract, { square, add } from "./utils.js";
 
 console.log("app.js is running!");
 console.log(square(4));
 console.log(add(4, 5));
+console.log(subtract(4, 5));
 
-import { isAdult, canDrink } from "./person.js";
+import isSenior, { isAdult, canDrink } from "./person.js";
 
 console.log("app.js is running!");
 console.log(isAdult(18));
 console.log(canDrink(18));
+console.log(isSenior(65));
